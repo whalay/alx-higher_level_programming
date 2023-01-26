cfb0bc8275424cb14ca50ec96608d7f1e4025701
Yusuf <soliuy5@gmail.com>
2023-01-25 21:48:02 -0800

    fix

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
 """ This module defines a square class with a private size attribute """
 
-class Square:
-    ''' Class Square object intialized with size '''
 
+class Square:
+    """ A square class with a private instance size attribute """
 
     def __init__(self, size=0):
-        ''' init method of class Square '''
-        if type(size) != int:
+        """ Initializes self """
+        if not isinstance(size, int):
             raise TypeError("size must be an integer")
         if size < 0:
             raise ValueError("size must be >= 0")
