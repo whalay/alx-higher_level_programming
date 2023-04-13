8c61cffbbf979a8162a99fcbdb7f03ac102d431b
Yusuf <soliuy5@gmail.com>
2023-04-13 09:57:39 -0700

    added

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -3,7 +3,7 @@
 const arg = parseInt(process.argv[2], 10);
 
 if (isNaN(arg)) {
-	console.log('Not a number');
+  console.log('Not a number');
 } else {
-	console.log('My number:', arg);
+  console.log('My number:', arg);
 }
