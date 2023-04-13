ddb0bc4c103a50438b98f77dbbb4c85d63400ab5
Yusuf <soliuy5@gmail.com>
2023-04-13 05:32:11 -0700

    argument

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,10 +1,10 @@
 #!/usr/bin/node
 const args = process.argv.length;
 
-if (args=== 0){
+if (args === 0) {
 	console.log('No Argument');
-} else if (args === 1){
+} else if (args === 1) {
 	console.log('Argument Found');
-} else{
+} else {
 	console.log('Arguments Found');
 }
