92d1c928be45378150e8d849e02987ee134912d3
Yusuf <soliuy5@gmail.com>
2023-04-13 08:58:04 -0700

    error

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,10 +1,10 @@
 #!/usr/bin/node
 const args = process.argv.length;
 
-if (args === 0) {
-	console.log('No Argument');
-} else if (args === 1) {
-	console.log('Argument Found');
+if (args <= 2) {
+  console.log('No argument');
+} else if (args === 3) {
+  console.log('Argument found');
 } else {
-	console.log('Arguments Found');
+  console.log('Arguments found');
 }
