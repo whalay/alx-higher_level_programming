cfc03fb5528f8eba8445956fc934365b871f5138
Yusuf <soliuy5@gmail.com>
2023-04-14 07:39:37 -0700

    added

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -1,12 +1,11 @@
 #!/usr/bin/node
-
 const args = parseInt(process.argv[2], 10);
 const myVar = 'X';
 
 if (isNaN(args)) {
-  console.log('Missing Size');
+  console.log('Missing size');
 } else {
-  for ( let i = 0; i < args; i++) {
+  for (let i = 0; i < args; i++) {
     console.log(myVar.repeat(args));
   }
 }
--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -3,7 +3,7 @@
 const args2 = parseInt(process.argv[2], 10);
 const args3 = parseInt(process.argv[3]. 10);
 
-function add(a,b) {
+function add(a, b) {
   return a + b;
 }
 
