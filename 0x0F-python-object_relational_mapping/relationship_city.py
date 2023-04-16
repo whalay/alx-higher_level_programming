d2f8087fe31b93a63dcc50c9d9d344883f5f2761
Yusuf <soliuy5@gmail.com>
2023-04-16 09:15:12 -0700

    added advance task

--- 0x0F-python-object_relational_mapping/relationship_city.py
+++ 0x0F-python-object_relational_mapping/relationship_city.py
@@ -5,9 +5,9 @@ Defines class City
 
 from sqlalchemy import Column, Integer, String, ForeignKey
 from sqlalchemy.ext.declarative import declarative_base
-from model_state import Base, State
+from relationship_state import Base, State
 
-Base = declarative_base()
+# Base = declarative_base()
 
 
 class City(Base):
